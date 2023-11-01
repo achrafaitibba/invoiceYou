35dde7cdbb87fdf1a390e0525d0b44146e1c1675
achrafaitibba <aitibbaachraf@gmail.com>
2023-11-01 13:01:03 +0100

    ,

--- src/main/java/com/onxshield/invoiceyou/invoicestatement/model/invoice.java
+++ src/main/java/com/onxshield/invoiceyou/invoicestatement/model/invoice.java
@@ -2,7 +2,6 @@ package com.onxshield.invoiceyou.invoicestatement.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
