c867e2236852d471c543bb1f585e090293f782b8
achrafaitibba <aitibbaachraf@gmail.com>
2023-11-05 12:04:00 +0100

    more edits

--- src/main/java/com/onxshield/invoiceyou/invoicestatement/repository/clientRepository.java
+++ src/main/java/com/onxshield/invoiceyou/invoicestatement/repository/clientRepository.java
@@ -1,9 +1,7 @@
 package com.onxshield.invoiceyou.invoicestatement.repository;
 
 import com.onxshield.invoiceyou.invoicestatement.model.client;
-import jdk.jfr.Registered;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface clientRepository extends JpaRepository<client, Long> {
