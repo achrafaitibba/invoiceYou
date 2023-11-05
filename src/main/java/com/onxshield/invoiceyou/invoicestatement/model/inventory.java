618aa0d98c500abe78672a5a88b9ba4975a6e21d
achrafaitibba <aitibbaachraf@gmail.com>
2023-11-05 12:02:08 +0100

    more edits

--- src/main/java/com/onxshield/invoiceyou/invoicestatement/model/inventory.java
+++ src/main/java/com/onxshield/invoiceyou/invoicestatement/model/inventory.java
@@ -1,7 +1,6 @@
 package com.onxshield.invoiceyou.invoicestatement.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
