52e75d102393e430d5d2b82eb21583cdd96b23fd
achrafaitibba <aitibbaachraf@gmail.com>
2023-11-05 12:04:42 +0100

    more edits

--- src/main/java/com/onxshield/invoiceyou/invoicestatement/request/productRequest.java
+++ src/main/java/com/onxshield/invoiceyou/invoicestatement/request/productRequest.java
@@ -1,14 +1,11 @@
 package com.onxshield.invoiceyou.invoicestatement.request;
 
-import com.onxshield.invoiceyou.invoicestatement.model.category;
 import com.onxshield.invoiceyou.invoicestatement.model.unit;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 @Data
 @Builder
 @AllArgsConstructor
