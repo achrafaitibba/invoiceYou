af5f6a81149fe2575244bad4556f93a03653a2bd
achrafaitibba <aitibbaachraf@gmail.com>
2023-11-08 13:09:22 +0100

    I'm really tired of thinking of the commit message, so i just push things I made...

--- src/main/java/com/onxshield/invoiceyou/invoicestatement/controller/clientController.java
+++ src/main/java/com/onxshield/invoiceyou/invoicestatement/controller/clientController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/client")
+@RequestMapping("/api/v1/clients")
 @RequiredArgsConstructor
 public class clientController {
 
