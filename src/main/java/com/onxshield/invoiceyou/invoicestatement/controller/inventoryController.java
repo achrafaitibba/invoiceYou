acf39c2a0980dc30d412bc8da5057c8a1167468c
achrafaitibba <aitibbaachraf@gmail.com>
2023-11-13 17:58:16 +0100

    all units endpoint done

--- src/main/java/com/onxshield/invoiceyou/invoicestatement/controller/inventoryController.java
+++ src/main/java/com/onxshield/invoiceyou/invoicestatement/controller/inventoryController.java
@@ -1,6 +1,7 @@
 package com.onxshield.invoiceyou.invoicestatement.controller;
 
 import com.onxshield.invoiceyou.invoicestatement.model.category;
+import com.onxshield.invoiceyou.invoicestatement.model.unit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,8 +11,14 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class inventoryController {
 
+
     @GetMapping("/categories")
     public ResponseEntity<category[]> getAllCategories(){
         return ResponseEntity.ok(category.values());
     }
+    @GetMapping("/units")
+    public ResponseEntity<unit[]> getAllUnits(){
+        return ResponseEntity.ok(unit.values());
+    }
+
 }
