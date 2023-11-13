dde1ed982ef1e1adc13ae787e6a5f6940e496331
achrafaitibba <aitibbaachraf@gmail.com>
2023-11-13 17:49:37 +0100

    all categories endpoint done

--- src/main/java/com/onxshield/invoiceyou/invoicestatement/controller/inventoryController.java
+++ src/main/java/com/onxshield/invoiceyou/invoicestatement/controller/inventoryController.java
@@ -1,10 +1,17 @@
 package com.onxshield.invoiceyou.invoicestatement.controller;
 
+import com.onxshield.invoiceyou.invoicestatement.model.category;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class inventoryController {
+
+    @GetMapping("/categories")
+    public ResponseEntity<category[]> getAllCategories(){
+        return ResponseEntity.ok(category.values());
+    }
 }
