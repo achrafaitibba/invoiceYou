5e5137a6344667e74fc7cf0e68e04edd57b9ff54
achrafaitibba <aitibbaachraf@gmail.com>
2023-11-13 21:26:23 +0100

    status endpoint

--- src/main/java/com/onxshield/invoiceyou/invoicestatement/controller/invoiceController.java
+++ src/main/java/com/onxshield/invoiceyou/invoicestatement/controller/invoiceController.java
@@ -1,10 +1,17 @@
 package com.onxshield.invoiceyou.invoicestatement.controller;
 
+import com.onxshield.invoiceyou.invoicestatement.model.status;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/invoices")
 @RequiredArgsConstructor
 public class invoiceController {
+
+    @GetMapping("/status")
+    public ResponseEntity<status[]> allStatus(){
+        return ResponseEntity.ok(status.values());
+    }
 }
