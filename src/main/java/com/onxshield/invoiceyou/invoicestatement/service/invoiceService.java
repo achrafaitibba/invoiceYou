db45328309ffef686a7fe36c0f84105f71a42a4b
achrafaitibba <aitibbaachraf@gmail.com>
2023-11-17 08:39:47 +0100

    ,

--- src/main/java/com/onxshield/invoiceyou/invoicestatement/service/invoiceService.java
+++ src/main/java/com/onxshield/invoiceyou/invoicestatement/service/invoiceService.java
@@ -2,8 +2,6 @@ package com.onxshield.invoiceyou.invoicestatement.service;
 
 
 import com.onxshield.invoiceyou.invoicestatement.dto.request.invoiceRequest;
-import com.onxshield.invoiceyou.invoicestatement.dto.request.merchandiseDetailsRequest;
-import com.onxshield.invoiceyou.invoicestatement.dto.request.merchandiseRequest;
 import com.onxshield.invoiceyou.invoicestatement.dto.response.basicInvoiceResponse;
 import com.onxshield.invoiceyou.invoicestatement.dto.response.merchandiseResponse;
 import com.onxshield.invoiceyou.invoicestatement.exceptions.requestException;
