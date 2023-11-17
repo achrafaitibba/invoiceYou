4cf361242c5dc38da10545d4b6ddfd38b37056c1
achrafaitibba <aitibbaachraf@gmail.com>
2023-11-17 08:41:56 +0100

    i'm just commiting sillythings to feel comfortable while watching the commits count increase hh

--- src/main/java/com/onxshield/invoiceyou/invoicestatement/repository/merchandiseRepository.java
+++ src/main/java/com/onxshield/invoiceyou/invoicestatement/repository/merchandiseRepository.java
@@ -2,11 +2,6 @@ package com.onxshield.invoiceyou.invoicestatement.repository;
 
 import com.onxshield.invoiceyou.invoicestatement.model.merchandise;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface merchandiseRepository extends JpaRepository<merchandise, Long> {
 
