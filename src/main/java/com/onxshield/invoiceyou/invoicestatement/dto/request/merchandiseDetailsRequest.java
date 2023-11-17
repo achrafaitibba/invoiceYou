86fe37d60f40a0d43835dc2fc09eceaf767bea57
achrafaitibba <aitibbaachraf@gmail.com>
2023-11-17 08:40:42 +0100

    ,

--- src/main/java/com/onxshield/invoiceyou/invoicestatement/dto/request/merchandiseDetailsRequest.java
+++
@@ -1,9 +0,0 @@
-package com.onxshield.invoiceyou.invoicestatement.dto.request;
-
-public record merchandiseDetailsRequest(
-        Long merchId,
-        Long productId,
-        Double quantity,
-        Double total
-) {
-}
